4e3feb835d715ead13c9e9f8b0ee4d326c391829
benoitTallandier <benoit.tallandier@gmail.com>
2017-01-24 13:15:04 +0100

    plus d'affichage de 2 questions

--- premiertest/question.php
+++ premiertest/question.php
@@ -75,7 +75,8 @@ else
 		echo "<meta http-equiv='refresh' content='0;URL=question.php'>";
 
 	}
-	else{		
+	else{
+		
 		$r = mysqli_query($db,"SELECT * FROM question");
 		$total = mysqli_num_rows($r);
 		$Query = "SELECT * FROM  question WHERE id_question NOT IN (SELECT ref_question FROM resultat WHERE id_user=".$_SESSION['id_user'].") ORDER BY RAND() ";
@@ -97,27 +98,27 @@ else
 				echo "</TABLE>";
 			echo "</div>";
 		}
-	}
-	else{
-		extract(mysqli_fetch_array($Result));
-		echo "<div id='bloc_question'>";
-			echo $question."</BR></br>";
-			$Result2 = mysqli_query($db,"SELECT * FROM reponse WHERE ref_question=".$id_question);
-		
-			echo "<form method='POST'>";
-				$iform = 0;
-				while($Line=mysqli_fetch_array($Result2)){
-					extract($Line);
-					echo "<div class='c_question' num=".$id_reponse."><input type='checkbox' class='reponse input_question' name='rep".$iform."' id='id_reponse".$id_reponse."' value='".$id_reponse."'>".$reponse."</div></br>";					
-					$iform+=1;
-				}
-				echo "<input type='hidden' name='nb_rep' value='".$iform."'>";
-				echo "<input type='hidden' name='question' value='".$id_question."'>";
-				echo "</br><input id='val_question' type='submit' name='valider' value='valider'>";						
-				echo "<input id='val_question' type='submit' name='passe' value='passer'>";						
-			echo "</form>";
-			$pour = ($total-$nombre)/$total *100;
-			echo "<div style='float:right;color:grey;'>".$pour."%,</div>";
-		echo "</div>";
+		else{
+			extract(mysqli_fetch_array($Result));
+			echo "<div id='bloc_question'>";
+				echo $question."</BR></br>";
+				$Result2 = mysqli_query($db,"SELECT * FROM reponse WHERE ref_question=".$id_question);
+			
+				echo "<form method='POST'>";
+					$iform = 0;
+					while($Line=mysqli_fetch_array($Result2)){
+						extract($Line);
+						echo "<div class='c_question' num=".$id_reponse."><input type='checkbox' class='reponse input_question' name='rep".$iform."' id='id_reponse".$id_reponse."' value='".$id_reponse."'>".$reponse."</div></br>";					
+						$iform+=1;
+					}
+					echo "<input type='hidden' name='nb_rep' value='".$iform."'>";
+					echo "<input type='hidden' name='question' value='".$id_question."'>";
+					echo "</br><input id='val_question' type='submit' name='valider' value='valider'>";						
+					echo "<input id='val_question' type='submit' name='passe' value='passer'>";						
+				echo "</form>";
+				$pour = ($total-$nombre)/$total *100;
+				echo "<div style='float:right;color:grey;'>".$pour."%,</div>";
+			echo "</div>";
+		}
 	}
 }
